833b7a3bc0cdbe54717d371f31c43d5bc884f032
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-02 12:42:31 -0500

    push

---
+++ Lab 4.3/src/GameCode.java
@@ -0,0 +1,12 @@
+/**Kaitlyn Cao & Lamia Islam
+ * Lab 4.3 Button Games*/
+import javafx.application.*;
+import javafx.stage.Stage;
+public class GameCode extends Application{
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My First JavaFX App");
+
+        primaryStage.show();
+    }
+
+}
