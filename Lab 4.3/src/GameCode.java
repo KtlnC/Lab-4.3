c9f1a1f8673b5023c3d2a3e5cf75fb58c5762dcd
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-03 12:28:04 -0500

    push

--- Lab 4.3/src/GameCode.java
+++ Lab 4.3/src/GameCode.java
@@ -1,7 +1,10 @@
 /**Kaitlyn Cao & Lamia Islam
  * Lab 4.3 Button Games*/
+import java.awt.Color;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
@@ -10,10 +13,19 @@ public class GameCode extends Application{
     public void start(Stage primaryStage) throws Exception 
     {
         primaryStage.setTitle("My First JavaFX App");
-        
+ 
+        Group root = new Group();
+    	Scene scene = new Scene(root, 200, 150);
+    	//scene.setFill(Color.LIGHT_GRAY);
+    	        
+       
         Button button = new Button("Click Me!");
         
-        Scene scene = new Scene(button, 200, 100);
+        /*
+        Scene scene = new Scene(button, 100, 50);
+        primaryStage.setScene(scene);
+        */
+        root.getChildren().add(button);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
