3e5e40f9fec18836e4db1cd0d2dee3adb70940de
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-03 12:01:30 -0500

    button works

--- Lab 4.3/src/GameCode.java
+++ Lab 4.3/src/GameCode.java
@@ -1,12 +1,26 @@
 /**Kaitlyn Cao & Lamia Islam
  * Lab 4.3 Button Games*/
-import javafx.application.*;
+import javafx.application.Application;
 import javafx.stage.Stage;
-public class GameCode extends Application{
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("My First JavaFX App");
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 
+public class GameCode extends Application{
+	
+    public void start(Stage primaryStage) throws Exception 
+    {
+        primaryStage.setTitle("My First JavaFX App");
+        
+        Button button = new Button("Click Me!");
+        
+        Scene scene = new Scene(button, 200, 100);
+        primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    
+    public static void main(String[] args)
+    {
+    	Application.launch(args);
     }
 
 }
