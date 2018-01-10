f95af971fce9b34edcb9bb8af223452e90b9eb12
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-10 12:45:11 -0500

    shove

--- Lab 4.3/src/GameCode.java
+++ Lab 4.3/src/GameCode.java
@@ -1,13 +1,19 @@
 /**Kaitlyn Cao & Lamia Islam
  * Lab 4.3 Button Games*/
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
-public class GameCode extends Application{
-	
+public class GameCode extends Application
+{
+	 private int count;
+	 
     public void start(Stage primaryStage) throws Exception 
     {
         primaryStage.setTitle("FastClicker");
@@ -16,6 +22,15 @@ public class GameCode extends Application{
     	Scene scene = new Scene(root, 200, 150);    	        
        
         Button button = new Button("Click Me!");
+        
+        button.setLayoutX(65);
+        button.setLayoutY(60);
+        
+        button.setOnAction(value ->
+        {
+        	count++;
+        	System.out.println(count);
+        });
         
         root.getChildren().add(button);
         primaryStage.setScene(scene);
@@ -26,5 +41,4 @@ public class GameCode extends Application{
     {
     	Application.launch(args);
     }
-
 }
